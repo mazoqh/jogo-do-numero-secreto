82073258bc48e53b55800eaeabb08794872f5234
patrickezera <40614649+mazoqh@users.noreply.github.com>
2024-02-20 12:23:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function verificarChute() {
     if (chute > numeroSecreto) {
       exibirTextoNaTela("p", "O número secreto é menor");
     } else {
-      exibirTextoNaTela("p", "O número secreito é maior");
+      exibirTextoNaTela("p", "O número secreto é maior");
     }
     tentativas++;
     limparCampo();
